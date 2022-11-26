f3677023699224fe457c564b472845b0e1f5f721
YaroslavLazorenko <lazorenko85@gmail.com>
2022-11-26 23:44:58 +0200

    Complete step 4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Command } = require("commander");
 const {
   listContacts,
   getContactById,
@@ -5,9 +6,62 @@ const {
   removeContact,
 } = require("./controllers/contacts");
 
-listContacts().then((data) => console.table(data));
-getContactById("1").then((data) => console.table(data));
-// addContact("Test", "test@test.com", "777-777-777").then((data) =>
-//   console.table(data)
-// );
-removeContact("92dc90a7-5732-4efd-b4ef-ce894e19b5d7");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      if (contacts) {
+        if (contacts.length === 0) {
+          return console.log("There are no items in contacts database");
+        }
+        console.table(contacts);
+      }
+      break;
+
+    case "get":
+      const contactById = await getContactById(id);
+      if (contactById) {
+        console.table(contactById);
+        return;
+      }
+      console.log("Contact with such id is not found");
+      break;
+
+    case "add":
+      const addedContact = await addContact(name, email, phone);
+      if (addedContact) {
+        console.log("The next contact was added:");
+        console.table(addedContact);
+        return;
+      }
+      console.log("Contact was no added");
+      break;
+
+    case "remove":
+      const removedContact = await removeContact(id);
+      if (removedContact) {
+        console.log("The next contact was deleted:");
+        console.table(removedContact);
+        return;
+      }
+      console.log("Contact with such id is not found");
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+(async () => await invokeAction(argv))();
