9a671db3bf35952d46330a8eaea0a517edf1f13e
YaroslavLazorenko <lazorenko85@gmail.com>
2022-12-11 04:20:00 +0200

    Connect mongoose and add environment variables

---
+++ libs/consts.js
@@ -0,0 +1,5 @@
+const DEFAULT_SERVER_PORT = 3000;
+
+module.exports = {
+  DEFAULT_SERVER_PORT,
+};
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+const { DEFAULT_SERVER_PORT } = require("./libs/consts");
+
+mongoose.set("strictQuery", false);
+
+const { DB_HOST, PORT = DEFAULT_SERVER_PORT } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
