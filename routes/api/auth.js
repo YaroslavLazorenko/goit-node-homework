4487f908fba78328a066471e97614d818c10bb40
YaroslavLazorenko <lazorenko85@gmail.com>
2022-12-25 14:51:11 +0200

    Add login router and controller

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { signup } = require("../../controllers/auth");
+const { signup, login } = require("../../controllers/auth");
 const { authSchema } = require("../../schemas/user");
 const { isValidId, validateBody } = require("../../middlewares");
 const { ctrlWrapper } = require("../../helpers");
@@ -8,5 +8,7 @@ const { ctrlWrapper } = require("../../helpers");
 const router = express.Router();
 
 router.post("/signup", validateBody(authSchema), ctrlWrapper(signup));
+
+router.post("/login", validateBody(authSchema), ctrlWrapper(login));
 
 module.exports = router;
