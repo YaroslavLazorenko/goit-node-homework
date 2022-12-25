7b0c93a0c6a97cb05ff2bd24a6396ac3e6217db4
YaroslavLazorenko <lazorenko85@gmail.com>
2022-12-26 00:47:46 +0200

    Fix getAll controller

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -5,8 +5,14 @@ const getAll = async (req, res) => {
   const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
 
+  let filter = null;
+  if (favorite === "false" || favorite === "true") {
+    const isFavoriteTrue = favorite === "true";
+    filter = { favorite: isFavoriteTrue };
+  }
+
   const result = await Contact.find(
-    { owner, favorite },
+    { owner, ...filter },
     "-createdAt -updatedAt",
     {
       skip,
