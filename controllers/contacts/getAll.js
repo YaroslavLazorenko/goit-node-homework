6a04798db302922fbbad775c67936da7567e6d6d
YaroslavLazorenko <lazorenko85@gmail.com>
2023-01-13 22:43:49 +0200

    Add fields in User model, update getAll controller

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -5,20 +5,13 @@ const getAll = async (req, res) => {
   const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
 
-  let filter = null;
-  if (favorite === "false" || favorite === "true") {
-    const isFavoriteTrue = favorite === "true";
-    filter = { favorite: isFavoriteTrue };
-  }
+  const query = { owner };
+  query.favorite = favorite || { $in: [true, false] };
 
-  const result = await Contact.find(
-    { owner, ...filter },
-    "-createdAt -updatedAt",
-    {
-      skip,
-      limit,
-    }
-  ).populate("owner", "email");
+  const result = await Contact.find(query, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
 
   res.json(result);
 };
